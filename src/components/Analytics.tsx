840baf18c57430b6ba92d16d228bdae90f65c9e5
cquijano <carlos.quijano@gmail.com>
2026-04-03 13:31:30 -0600

    feat(v1.5): create GA4 component and append it to the main layout

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,21 @@
+import Script from 'next/script'
+
+export default function Analytics() {
+    return (
+        <>
+            {/* Google Analytics Radar */}
+            <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                `}
+            </Script>
+        </>
+    )
+}
